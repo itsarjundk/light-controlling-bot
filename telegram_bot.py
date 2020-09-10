7d91413dfcc3722fa390915d4c3763509cd779e9
itsarjundk <69899740+itsarjundk@users.noreply.github.com>
2020-09-10 12:57:51 +0530

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -7,6 +7,12 @@ from Adafruit_IO import Client, Feed , Data
 
 aio = Client(os.getenv('ADAFRUIT_IO_USERNAME'),os.getenv('ADAFRUIT_IO_KEY'))
 
+aio = Client(x,y)
+data = aio.data('bot')
+def last(bot,update):
+    for d in data:
+        chat_id = update.message.chat_id
+        bot.send_message(chat_id,'Data value: {0}'.format(d.value))
 def light_on(bot,update):
     value= Data(value=1)
     value_send=aio.create_data('bot',value)
@@ -27,15 +33,16 @@ def get_url():
     return url
 
 def dog(bot,update):
-    print('got dog')
     url = get_url()
     chat_id = update.message.chat_id
     bot.send_photo(chat_id,photo=url)
+
 
 u = Updater('1368495354:AAEs2hs_nVNjhgwog36BOMIS3jX4raZ3bLQ')
 dp = u.dispatcher
 dp.add_handler(CommandHandler('dog',dog))
 dp.add_handler(CommandHandler('light_on',light_on))
 dp.add_handler(CommandHandler('light_off',light_off))
+dp.add_handler(CommandHandler('last_10',last))
 u.start_polling()
 u.idle()
