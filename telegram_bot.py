d0c4d49cadd35fe2857df9b42a6425578be63a05
itsarjundk <69899740+itsarjundk@users.noreply.github.com>
2020-09-10 13:16:26 +0530

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -46,7 +46,6 @@ dp = u.dispatcher
 dp.add_handler(CommandHandler('dog',dog))
 dp.add_handler(CommandHandler('light_on',light_on))
 dp.add_handler(CommandHandler('light_off',light_off))
-for i in range(100):
-    dp.add_handler(CommandHandler('last_'+str(i),last(i))
+for i in range(100): dp.add_handler(CommandHandler('last_'+str(i),last(i))
 u.start_polling()
 u.idle()
